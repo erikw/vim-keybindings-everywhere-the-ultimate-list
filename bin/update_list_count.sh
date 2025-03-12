08b43e1513f11822ef25c20c3de30a7af22849fd
Erik Westrup <erik.westrup@icloud.com>
2025-03-12 07:20:55 +0000

    Extract filename to constant

--- bin/update_list_count.sh
+++ bin/update_list_count.sh
@@ -6,6 +6,8 @@ set -o nounset
 set -o pipefail
 [[ "${TRACE-0}" =~ ^1|t|y|true|yes$ ]] && set -o xtrace
 
+FILE="README.md"
+
 MARKER_NATIVE="<magic-marker-nbr-native>"
 MARKER_EXTENSIONS="<magic-marker-nbr-extensions>"
 
@@ -17,7 +19,7 @@ SIGN_EXTENSIONS=:heavy_plus_sign:
 
 count_occurences() {
 	local sign="$1"
-	c=$(grep --count "$sign" README.md)
+	c=$(grep --count "$sign" "$FILE")
 	echo $(($c-1))  # Listed in explanation - does not count!
 }
 
@@ -27,8 +29,8 @@ update_count() {
 	local search="$3"
 
 	img_tag=$(printf "$template" "$count")
-	sed -i.bak -e "s|^.*${search}.*$|${img_tag}|" README.md
-	test -e README.md.bak && rm README.md.bak
+	sed -i.bak -e "s|^.*${search}.*$|${img_tag}|" "$FILE"
+	test -e "${FILE}.bak" && rm "${FILE}.bak"
 }
 
 count_native=$(count_occurences $SIGN_NATIVE)
